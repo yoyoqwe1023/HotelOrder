cee4b39a8c60d07a737ebc6a696f3fdd5fc46099
yoyoqwe1023 <yoyoqwe1023@gmail.com>
2023-04-27 15:22:34 +0800

    修改CSS

---
+++ HotelOrder/Controllers/RoomController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelOrder.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: Room
+        public ActionResult List()
+        {
+            HotelOrderEntities db = new HotelOrderEntities();
+            var datas = from c in db.Room
+                        select c;
+            return View(datas);
+        }
+    }
+}
